fe6371bdd29f1380265202072f355904bf873938
PNFeather <921192114@qq.com>
2019-07-02 17:50:29 +0800

    提交

--- src/page/PK/detail.vue
+++ src/page/PK/detail.vue
@@ -1,8 +1,16 @@
 <template>
-  <div name='detail'>
-    <header>PK</header>
+  <div name='detail' class="fillcontain">
+    <header class="header">PK</header>
     <section class="groups">
-      <div class="item">a <i class="iconfont iconvs"></i> b</div>
+      <div class="group" v-for="(group, groupIndex) in groups" :key="groupIndex">
+        <div class="item" v-for="(item, itemIndex) in group" :key="itemIndex">
+          <div class="name">{{item.name1}}</div>
+          <div class="icon">
+            <i class="iconfont iconvs"></i>
+          </div>
+          <div class="name">{{item.name2}}</div>
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -13,15 +21,16 @@
     name: 'detail',
     data () {
       return {
+        groups: []
       };
     },
     created () {
       pkDetail().then(res => {
-        console.log(res);
         let data = res.data;
         if (data.code == 0) {
           let reData = data.data;
-          console.log(reData);
+          this.groups = [...reData];
+          console.log(this.groups);
         } else {
           this.$message.error(data.message);
         }
@@ -36,5 +45,49 @@
 </script>
 <style scoped lang="less">
   @import '~@/style/mixin';
-  [name = 'detail']{}
+  [name = 'detail']{
+    display: flex;
+    flex-direction: column;
+    .header{
+      .wh(100%, 120px);
+      .ft(36px, 120px);
+      padding-left: 70px;
+      border-bottom: 1px solid #BBBBBB;
+    }
+    .groups{
+      padding: 29px;
+      flex: 1;
+      display: flex;
+      .group:last-child{
+        border: none!important;
+      }
+      .group{
+        flex: 1;
+        box-sizing: border-box;
+        border-right: 1px solid #BBBBBB;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        .item{
+          width: 100%;
+          font-size: 31px;
+          display: flex;
+          .icon{
+            flex: 47px 0 0;
+            .fac();
+            .iconfont{
+              font-size: 27px;
+              color: #FCCC60;
+            }
+          }
+          .name{
+            text-align: center;
+            display: inline-block;
+            flex: 1;
+          }
+        }
+      }
+    }
+  }
 </style>
