9105cec46d487a58c8c0c22c097456c82ac63371
PNFeather <921192114@qq.com>
2019-07-02 15:26:44 +0800

    提交

---
+++ src/page/PK/underway.vue
@@ -0,0 +1,16 @@
+<template>
+  <div name='underway'>
+    pk中
+  </div>
+</template>
+
+<script type='text/babel'>
+  export default {
+    name: 'underway',
+    mounted () {
+    }
+  };
+</script>
+<style scoped lang="less">
+  [name = 'underway']{}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
 
 Vue.use(Router);
+
+// PK中
+const underway = r => require.ensure([], () => r(require('@/page/PK/underway')), 'underway');
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '*',
+      redirect: '/underway'
+    }, {
+      path: '/underway',
+      component: underway
     }
   ]
 });
