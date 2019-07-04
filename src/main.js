9feeab0f38b09479d17f788065dc28d1bc6d34a2
PNFeather <921192114@qq.com>
2019-07-04 14:20:30 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -8,6 +8,15 @@ import 'nprogress/nprogress.css';
 
 (process.env.BASE_URL == '/api') && process.env.MOCK && require('@/mock');
 
+let setRem = () => {
+  let whdef = 10 / 1450;// 表示1450的设计图,使用10PX的默认值
+  let bodyWidth = document.body.clientWidth;// 当前窗口的宽度
+  let rem = bodyWidth * whdef;// 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值
+  document.getElementsByTagName('html')[0].style.fontSize = rem + 'px';
+};
+window.addEventListener('load', setRem);
+window.addEventListener('resize', setRem);
+
 NProgress.configure({
   easing: 'ease', // 动画方式
   speed: 500, // 递增进度条的速度
